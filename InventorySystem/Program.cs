9cb7408929cce15e0889bfca48962f4ae6ddc450
durmazertugrul <durmazer.tugrul.42@gmail.com>
2026-08-12 21:37:42 +0300

    Add actual class implementations (previous commit had unsaved placeholder code)

--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -1,1 +1,86 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Inventory 
+{ 
+    //Envanter yapısının genel metotları yönetilecek. show, add, remove, use, search, sort, 
+    List<Item> items = new List<Item>();
+
+    public void AddItem() { }
+    public void RemoveItem() { }
+    public void FindItem() { }
+    public void UseItem() { }
+    public void ShowInventory() { }
+    public void SortItem() { }
+    public void ShowItemDetail() { }
+}
+
+class Item 
+{ //Item genel bilgileri tutulacak
+    private int id;
+    private string name;
+    private int prive;
+    private float weight;
+
+
+}
+
+class InventoryManager 
+{
+    //İşlemler, menüler, genel yönetimsel bilgiler burada olacak. 
+
+    public void MainMenu() 
+    {
+        /*
+         * Genel görünüm:
+            ================================
+                    INVENTORY SYSTEM
+            ================================
+
+            1. Show Inventory
+            2. Add Item
+            3. Remove Item
+            4. Use Item
+            5. Search Item
+            6. Sort Inventory
+            7. Show Item Details
+            8. Exit
+
+            Select:
+         */
+    }
+}
+
+class Weapon : Item
+{ 
+    //Weapon genel bilgileri tutulacak
+    private int damageValue;
+}
+
+class Armor : Item
+{
+    //Armor genel bilgileri tutulacak
+
+    private int defenseValue;
+
+
+}
+
+class Potion : Item
+{
+    //Potion genel bilgileri tutulacak
+
+    private int healAmount;
+}
+
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
